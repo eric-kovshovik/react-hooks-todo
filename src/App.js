186dd91cd8d998defb385b1a5b891d7a4fef724d
Eric Kovshovik <jaroslav.kovshovik@gmail.com>
2019-03-15 17:58:42 -0600

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,43 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
-
-function Todo({ todo, index, completeTodo, removeTodo }) {
-  return (
-    <div
-      className="todo"
-      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
-    >
-      {todo.text}
-
-      <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
-        <button onClick={() => removeTodo(index)}>x</button>
-      </div>
-    </div>
-  );
-}
-
-function TodoForm({ addTodo }) {
-  const [value, setValue] = useState("");
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue("");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-    </form>
-  );
-}
+import Todo from "./Todo";
+import TodoForm from "./TodoForm"
 
 function App() {
   const [todos, setTodos] = useState([
